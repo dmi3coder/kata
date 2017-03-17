7a9a50628f08c0c71188045259faaab2bf58fbfc
dim3coder <dmi3coder@gmail.com>
2017-03-17 23:01:25 +0200

    kata 006: find smallest word length

--- kata006/src/io/github/dmi3coder/Kata.java
+++ kata006/src/io/github/dmi3coder/Kata.java
@@ -1,6 +1,8 @@
 package io.github.dmi3coder;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * Created by dim3coder on 3/3/17.
@@ -15,8 +17,19 @@ public class Kata {
     });
     return splittedStrings[0].length();
   }
+  public static int[] array = {8,1,9,2,5,8,11,42,8323,82,1,66,55,6,8};
 
   public static void main(String[] args) {
-    System.out.println(FindShort("123 1234 1234567 123 12"));
+    long begin = System.nanoTime();
+    Arrays.sort(array);
+    for (int i = 0; i < array.length-1; i++) {
+      if(array[i] == array[i+1]){
+        System.out.println(array[i]);
+      }
+    }
+    long diff = System.nanoTime() -begin;
+    System.out.println("Diff : "+diff);
   }
 }
+// 1207565
+//  610247
